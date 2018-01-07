9ce7feb320d27336da155b9ddfcf760366e4e3a2
xingkaizhu <18990361729@qq.com>
2018-01-07 14:34:50 +0800

    i

---
+++ Sixteen decimal decimal system .c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int hexToDecimal(int m);
+int main (){
+	int a;
+	scanf("%x",&a);
+	hexToDecimal(a);
+	return 0;
+	
+	
+}
+int hexToDecimal(int m){
+	printf("%d",m);
+	return 0;		
+}
